e14e6c2c93609afa598fc6a58db467bb62f4e18f
pali <p.mikulaj@gmail.com>
2023-04-23 00:55:19 +0200

    moving and clicking, but moving is very slow

--- main.py
+++ main.py
@@ -14,44 +14,73 @@ movement_direction = None
 movements = {'up': [-1, 0], 'down': [1, 0], 'left': [0, -1], 'right': [0, 1],
              'up left': [-1, -1], 'up right': [-1, 1], 'down left': [1, -1], 'down right': [1, 1],
              'stop': [0, 0]}
-
+clicks = {'click': [1, 'left'], 'double click': [2, 'left'],
+          'right click': [1, 'right'], 'right double click': [2, 'right'],
+          'middle click': [1, 'middle']}
+click_count = None
+click_button = None
 screen_w, screen_h = pyautogui.size()
 lock = threading.Lock()
 
 
 def do_movement():
     global movement_direction
+    print('doin movement')
     while True:
         if movement_direction is not None:
             pyautogui.move(movement_direction[1], movement_direction[0])
 
 
+def click():
+    global click_count
+    global click_button
+    print(f'click count is {click_count}')
+    print(f'click button is {click_button}')
+    while True:
+        if click_count is not None and click_button is not None:
+            print('i am doing some clicking')
+            pyautogui.click(clicks=click_count, button=click_button)
+            click_count = None
+            click_button = None
+
+
 def set_variable(text_to_command_queue):
     global movement_direction
+    global click_count
+    global click_button
     while True:
         if not text_to_command_queue.empty():
-            try:
-                movement_direction = movements[text_to_command_queue.get()]
-            except KeyError:
-                print('unknown command')
+            command = text_to_command_queue.get()
+            if command in movements:
+                movement_direction = movements[command]
+            elif command in clicks:
+                print(f'command in clicks is {command}')
+                click_count = clicks[command][0]
+                click_button = clicks[command][1]
+                print(click_count)
+                print(click_button)
+            else:
+                print(command)
         # time.sleep(5)
 
 
 if __name__ == '__main__':
+    # time.sleep(5)
+    # pyautogui.click(clicks=1, button='left')
     # Create the threads
-    thread1 = threading.Thread(target=set_variable, args=[text_to_command_queue_])
-    thread2 = threading.Thread(target=do_movement)
-    thread3 = threading.Thread(target=get_voice_command, args=[text_to_command_queue_])
-    thread1.daemon = True
-    thread2.daemon = True
-    thread3.daemon = True
+    thread_set_var = threading.Thread(target=set_variable, daemon=True, args=[text_to_command_queue_])
+    thread_do_movement = threading.Thread(target=do_movement, daemon=True)
+    thread_click = threading.Thread(target=click, daemon=True)
+    thread_get_voice_command = threading.Thread(target=get_voice_command, daemon=True, args=[text_to_command_queue_])
 
     # Start the threads
-    thread1.start()
-    thread2.start()
-    thread3.start()
+    thread_set_var.start()
+    thread_do_movement.start()
+    thread_click.start()
+    thread_get_voice_command.start()
 
     # Wait for the threads to finish
-    thread1.join()
-    thread2.join()
-    thread3.join()
+    thread_set_var.join()
+    thread_do_movement.join()
+    thread_click.join()
+    thread_get_voice_command.join()
